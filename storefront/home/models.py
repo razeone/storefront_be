b5241837052630ff82dd4d0ebb77241e2f45f4e1
Jorge Raze <jorge.raze@clevertech.biz>
2016-10-13 14:34:19 -0500

    Working with templates and homepage model

--- storefront/home/models.py
+++ storefront/home/models.py
@@ -5,7 +5,22 @@ from django.contrib.auth.models import User
 from django.utils.timezone import now
 
 
+class HomePage(models.Model):
+    """
+    The settings for the homepage
+    """
+    page_title = models.CharField(max_length=200, null=False)
+    logo_file = models.CharField(max_length=200, null=False)
+    description = models.CharField(max_length=500)
+    keywords = models.CharField(max_length=500)
+    copyright_message = models.CharField(max_length=400, null=False)
+    is_active = models.BooleanField(default=True)
+
+
 class UserProfile(models.Model):
+    """
+    The user profile's model
+    """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     created_date = models.DateTimeField('created date',
                                         default=now()
